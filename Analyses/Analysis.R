6963a10abfca58f2f972bf995fb174bd262dddf3
wrmeyer <56975717+wrmeyer@users.noreply.github.com>
2022-04-05 16:20:27 -0400

    Fixing sync issue on repository

--- Analyses/Analysis.R
+++ Analyses/Analysis.R
@@ -1,6 +1,6 @@
 library(readr)
 library(tidyverse)
-tweets <- read_csv("C:/Users/wilmd/OneDrive/Desktop/python_env/env/Collegiate_News_and_Sexual_Misconduct/Tweets/tweets_cleaned.csv", 
+tweets <- read_csv("C:/Users/Wil/Desktop/GetOldTweets/TwitterSP2022/Collegiate_News_and_Sexual_Misconduct/Tweets/tweets_cleaned.csv", 
                            col_types = cols(X1 = col_skip(), `Unnamed: 0` = col_skip(), 
                                             geo = col_skip(), id = col_character(), 
                                             public_metrics = col_skip(), author_id = col_character(), 
@@ -25,10 +25,10 @@ View(by_day)
 #Histograms
 #########################
 df <- tweets %>%
-  filter(location == "Wisconsin")
+  filter(location == "UMD")
 ggplot(df, aes(x = created_at)) +
   geom_histogram(bins = 50) +
-  labs(title="Wisconsin Tweets")
+  labs(title="UMD Tweets")
 
 df <- articles %>%
   filter(location == "UMD") %>%
