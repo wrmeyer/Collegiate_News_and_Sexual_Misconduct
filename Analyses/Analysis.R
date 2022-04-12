379bba2c86796399616ae1e5f1ea3f4740e45761
wrmeyer <56975717+wrmeyer@users.noreply.github.com>
2022-04-12 18:30:51 -0400

    more analysis

--- Analyses/Analysis.R
+++ Analyses/Analysis.R
@@ -11,12 +11,15 @@ tweets <- read_csv("C:/Users/Wil/Desktop/GetOldTweets/TwitterSP2022/Collegiate_N
                                             in_reply_to_user_id = col_character(), 
                                             lang = col_skip(),
                                             attachments = col_skip()))
-articles <- read_csv("C:/Users/wilmd/OneDrive/Desktop/python_env/env/Collegiate_News_and_Sexual_Misconduct/Articles/articles_cleaned.csv", 
+articles <- read_csv("C:/Users/Wil/Desktop/GetOldTweets/TwitterSP2022/Collegiate_News_and_Sexual_Misconduct/Articles/articles_cleaned.csv", 
                             col_types = cols(X1 = col_skip(), 
                                              date = col_date(format = "%m/%d/%Y")))
-by_day <- read_csv("C:/Users/wilmd/OneDrive/Desktop/python_env/env/Collegiate_News_and_Sexual_Misconduct/Analyses/counts_by_day.csv", 
+by_day <- read_csv("C:/Users/Wil/Desktop/GetOldTweets/TwitterSP2022/Collegiate_News_and_Sexual_Misconduct/Analyses/counts_by_day.csv", 
                           col_types = cols(date = col_date(format = "%m/%d/%Y")))
 
+yearly_averages <- read_csv("C:/Users/Wil/Desktop/GetOldTweets/TwitterSP2022/Collegiate_News_and_Sexual_Misconduct/Analyses/yearly_averages.csv", 
+                            col_types = cols(...1 = col_skip(), date = col_date(format = "%Y-%m-%d")))
+View(yearly_averages)
 View(tweets)
 View(articles)
 View(by_day)
@@ -27,7 +30,7 @@ View(by_day)
 df <- tweets %>%
   filter(location == "UMD")
 ggplot(df, aes(x = created_at)) +
-  geom_histogram(bins = 50) +
+  geom_histogram(bins = 100) +
   labs(title="UMD Tweets")
 
 df <- articles %>%
@@ -42,11 +45,26 @@ ggplot(df, aes(x = date)) +
 ###########################
 
 counts <- by_day %>%
-  filter(location == "Stanford") %>%
-  filter(date > "2010-01-01")
+  filter(location == "Wisconsin") %>%
+  filter(date > "2012-01-01") %>%
+  filter(date < "2014-01-01")
 
 ggplot(counts, aes(date, tweet_count, colour = "Tweets")) + 
   scale_colour_manual("", breaks = c("Tweets", "Articles"),
                       values = c("Tweets" = "blue", "Articles" = "red")) +
   geom_smooth() +
-  geom_smooth(data = counts, aes(date, article_count, colour = "Articles"))
+  geom_smooth(data = counts, aes(date, article_count, colour = "Articles"))+
+  labs(title="Wisconsin")
+
+###########################
+#seasonality
+###########################
+averages <- yearly_averages %>%
+  filter(location == "Wisconsin")
+  
+
+ggplot(averages, aes(date, average_tweets, colour = "Blue")) + 
+  geom_smooth() + labs(title="Wisconsin Averages")
+  
+  # geom_smooth(data = averages, aes(date, average_articles, colour = "Articles"))+
+  # labs(title="Averages")
