796561a13227080d7af25d648935f69d4aff3a54
wrmeyer <56975717+wrmeyer@users.noreply.github.com>
2022-04-20 15:29:55 -0400

    Fixed decay curve for articles

--- Analyses/Analysis.R
+++ Analyses/Analysis.R
@@ -1,6 +1,7 @@
 library(readr)
 library(tidyverse)
 library(gghighlight)
+##########Desktop Import##############
 tweets <- read_csv("C:/Users/Wil/Desktop/GetOldTweets/TwitterSP2022/Collegiate_News_and_Sexual_Misconduct/Tweets/tweets_cleaned.csv", 
                            col_types = cols(X1 = col_skip(), `Unnamed: 0` = col_skip(), 
                                             geo = col_skip(), id = col_character(), 
@@ -19,6 +20,27 @@ by_day <- read_csv("C:/Users/Wil/Desktop/GetOldTweets/TwitterSP2022/Collegiate_N
                           col_types = cols(date = col_date(format = "%m/%d/%Y")))
 
 yearly_averages <- read_csv("C:/Users/Wil/Desktop/GetOldTweets/TwitterSP2022/Collegiate_News_and_Sexual_Misconduct/Analyses/yearly_averages.csv", 
+                            col_types = cols(...1 = col_skip(), date = col_date(format = "%Y-%m-%d")))
+
+##########Laptop Import##############
+tweets <- read_csv("C:/Users/wilmd/OneDrive/Desktop/python_env/env/Collegiate_News_and_Sexual_Misconduct/Tweets/tweets_cleaned.csv", 
+                   col_types = cols(X1 = col_skip(), `Unnamed: 0` = col_skip(), 
+                                    geo = col_skip(), id = col_character(), 
+                                    public_metrics = col_skip(), author_id = col_character(), 
+                                    conversation_id = col_character(), 
+                                    entities = col_skip(), possibly_sensitive = col_skip(), 
+                                    created_at = col_date(format = "%m/%d/%Y"), 
+                                    context_annotations = col_skip(), 
+                                    in_reply_to_user_id = col_character(), 
+                                    lang = col_skip(),
+                                    attachments = col_skip()))
+articles <- read_csv("C:/Users/wilmd/OneDrive/Desktop/python_env/env/Collegiate_News_and_Sexual_Misconduct/Articles/articles_cleaned.csv", 
+                     col_types = cols(X1 = col_skip(), 
+                                      date = col_date(format = "%m/%d/%Y")))
+by_day <- read_csv("C:/Users/wilmd/OneDrive/Desktop/python_env/env/Collegiate_News_and_Sexual_Misconduct/Analyses/counts_by_day.csv", 
+                   col_types = cols(date = col_date(format = "%m/%d/%Y")))
+
+yearly_averages <- read_csv("C:/Users/wilmd/OneDrive/Desktop/python_env/env/Collegiate_News_and_Sexual_Misconduct/Analyses/yearly_averages.csv", 
                             col_types = cols(...1 = col_skip(), date = col_date(format = "%Y-%m-%d")))
 View(yearly_averages)
 View(tweets)
@@ -59,14 +81,31 @@ ggplot(counts, aes(date, tweet_count, colour = "Tweets")) +
 
 
 ###########Effects Decay Correlations################
+require(lubridate)
+half_life_articles <- by_day %>%
+  filter(date > "2021-09-01") %>%
+  .$date %>% 
+  unique %>% 
+  map_dfr(~ {
+    by_day %>% 
+      filter(date <= .x) %>% 
+      group_by(location) %>% 
+      mutate(elapse = as.numeric(.x - date),
+             weighted = article_count*exp(1)^-(.05*elapse)) %>% 
+      summarise(hl_articles = sum(weighted)) %>%
+      mutate(date = .x) %>%
+      ungroup
+  })
+    
+
 effects_decay <- by_day %>%
-  filter(location == "UMD") %>%
+  filter(location == "Stanford") %>%
   filter(date > "2011-01-01")
 
 ggplot(effects_decay, aes(article_coverage, tweet_coverage)) +
   geom_smooth() +
   theme_classic() +
-  labs(x = "Collegiate News Coverage (Effects Decay Value)", y = "Tweet Coverage", title = "UMD", caption = "(p-value: 4.467e-05, cor: 0.06686)")
+  labs(x = "Collegiate News Coverage (Effects Decay Value)", y = "Tweet Coverage", title = "Stanford", caption = "(p-value: 4.467e-05, cor: 0.06686)")
 
 cor.test(effects_decay$article_coverage, effects_decay$tweet_coverage)
 
@@ -74,14 +113,15 @@ cor.test(effects_decay$article_coverage, effects_decay$tweet_coverage)
 ###########Seasonality################
 
 averages <- yearly_averages %>%
-  filter(location == "IUB") %>%
-  mutate(average_tweets = average_tweets*7)
+  filter(location == "UMN") %>%
+  mutate(across(c(average_tweets,coverage), ~ .x - mean(.x)/sd(.x)))
+  # mutate(average_tweets = average_tweets*7)
   
 
 ggplot(averages, aes(date, average_tweets, colour = "Tweets")) + 
   scale_colour_manual("", breaks = c("Tweets", "Collegiate News Coverage"),
                       values = c("Tweets" = "red", "Collegiate News Coverage" = "blue")) + 
-  geom_smooth() + labs(title="IUB Averages", y = "Averaged Values", x = "Date")+
+  geom_smooth() + labs(title="UMN Averages", y = "Averaged Values", x = "Date")+
   geom_smooth(data = averages, aes(date, coverage, colour = "Collegiate News Coverage"))+
   geom_vline(xintercept = as.numeric(as.Date("1999-05-07")), lwd = 1)+
   geom_vline(xintercept = as.numeric(as.Date("1999-08-20")), lwd = 1)+
